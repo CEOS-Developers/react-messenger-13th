927c98df15e1e8439f97a42a9e7b3ca72e29be97
Youngwoo Kim <yw.sean.kim@gmail.com>
2021-03-22 02:39:25 +0900

    chore: add ChatMessage to git

---
+++ src/components/ChatMessage.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from 'styled-components'
+import ProfilePicture from './ProfilePicture';
+
+const StyledChatMessage = styled.div`
+  margin-bottom: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  &.right {
+    flex-direction: row-reverse;
+  }
+`;
+
+const MessageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  .right & {
+    align-items: flex-end;
+  }
+`;
+
+const UserName = styled.div`
+  color: #144E9A;
+  margin-bottom: 8px;
+`;
+
+const Message = styled.div`
+  background-color: white;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+  padding: 10px;
+  max-width: 350px;
+`;
+
+export default function ChatMessage({ align }) {
+  return (
+    <StyledChatMessage className={ align }>
+      <ProfilePicture />
+      <MessageContainer>
+        <UserName>김영우</UserName>
+        <Message>안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요</Message>
+      </MessageContainer>
+    </StyledChatMessage>
+  )
+}
