dbf7e32f8a57aae1374b9f788f883377cdddb90f
park ye jin <dyddyd0514@naver.com>
2021-03-27 00:46:28 +0900

    Style messenger screen

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <BackGround >
       <TopBar />
-      <MessageBubble />
+      <MessageBox />
       <MessageInputBar />
     </BackGround>
   );
@@ -16,22 +16,77 @@ const BackGround = styled.div`
 `;
 
 const TopBox = styled.div`
+  background-color: lightgrey;
   color: grey;
   display : flex;
-  padding: 3%;
+  padding: 2%;
   border-bottom : solid 1px grey;
 `;
 
 const ProfileImage = styled.img`
-  width: 65px;
-  height : 100%;
+  width: 60px;
+  height : 60px;
   border-radius: 70%;
   overflow: hidden;
 `;
 const ProfileName = styled.div`
   padding-left: 20px;
-  line-height: 65px;
+  line-height: 60px;
   font-size : 20px;
+`;
+
+const ChattingImage = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 70%;
+  overflow: hidden;
+  margin: 15px 10px 0px;  
+`;
+
+const ChattingContext = styled.div`
+  padding-left: 20px;
+  line-height: 28px;
+  font-size: 15px;
+  border : 1px solid lightgrey;
+  border-radius : 30%;
+  margin : 15px 0px 0px;
+  padding : 10px;
+`;
+const ChattingBubble = styled.div`
+  display: flex;
+`;
+
+const MessageInputContainer = styled.div`
+  color: grey;
+  display : flex;
+  margin: 3% 0%;
+  border-top : solid 1px grey;
+  align-items: stretch;
+`;
+
+const MessageInputBox = styled.input`
+  width: 90%;
+  height: 26px;
+  margin : 2% 0% 2% 2%;
+  border: 0.5px lightgray solid;
+  text-align: center;
+  border-radius: 30px;
+  &:focus{
+    outline: none;
+  }
+`;
+const MessageInputButton = styled.button`
+  width: 10%;
+  height: 30px;
+  border-radius: 10px;
+  border: 0.5px lightgray solid;
+  text-align: center;
+  margin: 2%;
+  &:focus{
+    outline: none;
+    border: 0.5px gray solid;
+    background-color: lightgray;
+  }
 `;
 
 function TopBar(props) {
@@ -45,9 +100,18 @@ function TopBar(props) {
 
 function MessageBubble(props) {
   return (
+    <ChattingBubble>
+      <ChattingImage src={process.env.PUBLIC_URL + "/img.jpg"} alt="" />
+      <ChattingContext>hello</ChattingContext>
+    </ChattingBubble>
+  );
+}
+function MessageBox(props) {
+  return (
     <div>
-      <img src={process.env.PUBLIC_URL + "/img.jpg"} alt="" width="50px" height="50px;" />
-      <div>hello</div>
+      <MessageBubble />
+      <MessageBubble />
+
     </div>
   );
 }
@@ -59,10 +123,10 @@ function MessageInputBar(props) {
   }
 
   return (
-    <div>
-      <input placeholder="message"></input>
-      <button onClick={clickInputButton}>push</button>
-    </div>
+    <MessageInputContainer>
+      <MessageInputBox placeholder="message"></MessageInputBox>
+      <MessageInputButton onClick={clickInputButton}>push</MessageInputButton>
+    </MessageInputContainer>
   );
 }
 
