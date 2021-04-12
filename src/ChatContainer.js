f5372af96b98b170ffed63eac55fb8b568473cfc
SojeongLee <sojunglsj@naver.com>
2021-04-12 23:12:09 +0900

    docs : add .prettierrc file

--- src/ChatContainer.js
+++ src/ChatContainer.js
@@ -6,7 +6,6 @@ import Message from './Message';
 const StyledChatContainer = styled.div`
         display: flex;
         flex-direction: column;
-        
         height: 100%;
         width: 100%;
         padding-top: 150px;
@@ -22,7 +21,6 @@ const ChatContainer = ({ message, user }) => {
     window.scrollBy(0, document.body.scrollHeight);
 })
     return(
-
         <StyledChatContainer>
             {message.map(text_input =>(
                 <Message message={text_input} key={text_input.id} user={user}/>
